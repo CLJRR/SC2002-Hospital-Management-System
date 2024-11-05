1c0605dd2165054a560e99860ddcba3344a2fd5f
CLJR <calvinleejiarong@gmail.com>
2024-11-05 23:17:14 +0800

    deleted all class files

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,8 @@
 
 import UserSystem.*;
+import enums.*;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 
 public class Main {
@@ -12,13 +14,13 @@ public class Main {
         // Cast the result of load() to List<Staff>
         @SuppressWarnings("unchecked")
         List<User> users = (List<User>) userService.load(USER_FILE_NAME);
-//         users.add(new User("P1002", "Bob Stone", LocalDate.parse("1975-11-22"), Gender.MALE, "123456789", "bob.stone@example.com", "B+", Role.PATIENT));
-//         users.add(new User("P1003", "Charlie White", LocalDate.parse("1990-07-08"), Gender.MALE, "123456789", "charlie.white@example.com", "O-", Role.PATIENT));
-//         users.add(new User("D001", "John Smith", Gender.MALE, 45, Role.DOCTOR));
-//         users.add(new User("D002", "Emily Clarke", Gender.FEMALE, 38, Role.DOCTOR));
-// // Pharmacist and Administrator with age, gender, and role
-//         users.add(new User("P001", "Mark Lee", Gender.MALE, 29, Role.PHARMACIST));
-//         users.add(new User("A001", "Sarah Lee", Gender.FEMALE, 40, Role.ADMINISTRATOR));
+        users.add(new User("P1002", "Bob Stone", LocalDate.parse("1975-11-22"), Gender.MALE, "123456789", "bob.stone@example.com", "B+", Role.PATIENT));
+        users.add(new User("P1003", "Charlie White", LocalDate.parse("1990-07-08"), Gender.MALE, "123456789", "charlie.white@example.com", "O-", Role.PATIENT));
+        users.add(new User("D001", "John Smith", Gender.MALE, 45, Role.DOCTOR));
+        users.add(new User("D002", "Emily Clarke", Gender.FEMALE, 38, Role.DOCTOR));
+// Pharmacist and Administrator with age, gender, and role
+        users.add(new User("P001", "Mark Lee", Gender.MALE, 29, Role.PHARMACIST));
+        users.add(new User("A001", "Sarah Lee", Gender.FEMALE, 40, Role.ADMINISTRATOR));
 
         // System.out.println("Please enter UserId");
 

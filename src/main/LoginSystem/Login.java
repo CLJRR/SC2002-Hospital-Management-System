9734414865366aa3d284a0dce310bacd57d4bb92
exkitty1 <ngziyunalol@gmail.com>
2024-11-06 12:57:33 +0800

    updated login

--- src/main/LoginSystem/Login.java
+++ src/main/LoginSystem/Login.java
@@ -1,50 +1,13 @@
 package LoginSystem;
+import UserSystem.User;
+import UserSystem.UserService;
 import enums.*;
-import java.util.Scanner;
+import java.io.IOException;
+import java.util.List;
+
 //comment
 public class Login {
     private Role choice;
-
-    public void login() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to HMS");
-        System.out.println("Select option: ");
-        System.out.println("[1] Administrator");
-        System.out.println("[2] Pharmacist");
-        System.out.println("[3] Doctor");
-        System.out.println("[4] Patient");
-        int option;
-        int valid = 0;
-        option = 0;
-        while (valid != 1) {
-            System.out.println("Enter an option: ");
-            option = sc.nextInt();
-            if (option == 1 || option == 2 || option == 3 || option == 4) {
-                valid = 1;
-                break;
-            }
-            System.out.println("Error! Please enter either [1] or [2] or [3] or [4]");
-        }
-
-        switch (option) {
-            case 1 -> {
-                setChoice(Role.ADMINISTRATOR);
-                System.out.println("Welcome Administrator");
-            }
-            case 2 -> {
-                setChoice(Role.PHARMACIST);
-                System.out.println("Welcome Pharmacist");
-            }
-            case 3 -> {
-                setChoice(Role.DOCTOR);
-                System.out.println("Welcome Doctor");
-            }
-            case 4 -> {
-                setChoice(Role.PATIENT);
-                System.out.println("Welcome Patient");
-            }
-        }
-    }
 
     public Role getChoice() {
         return this.choice;
@@ -52,5 +15,29 @@ public class Login {
 
     public void setChoice(Role choice) {
         this.choice = choice;
+    }
+    
+    public boolean checkAttempt(UserLogin login) throws IOException {
+        final String FILE_NAME = "./data/users.txt";
+        UserService userService = new UserService();
+
+        @SuppressWarnings("unchecked")
+        List<User> users = (List<User>) userService.load(FILE_NAME);
+
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getId().equals(login.getLoginIDAttempt())) {
+                if (users.get(i).getPassword().equals(login.getPasswordAttempt())) {
+                    System.out.println("Login successfully.");
+                    this.setChoice(users.get(i).getRole());
+                    System.out.println("Welcome " + this.choice);
+                    return true;
+                } else {
+                    System.out.println("Incorrect Password.");
+                    return false;
+                }
+            }
+        }
+        System.out.println("LoginID not found.");
+        return false;
     }
 }
--- src/main/LoginSystem/UserLogin.java
+++ src/main/LoginSystem/UserLogin.java
@@ -1,10 +1,5 @@
 package LoginSystem;
 
-import UserSystem.User;
-import UserSystem.UserService;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class UserLogin {
@@ -36,38 +31,5 @@ public class UserLogin {
         System.out.println("Enter password: ");
         String pw = sc.nextLine();
         setPasswordAttempt(pw);
-    }
-    
-    public boolean checkAttempt(Login login) throws IOException {
-        final String FILE_NAME = "./data/users.txt";
-        UserService userService = new UserService();
-        
-        @SuppressWarnings("unchecked")
-        List<User> users = (List<User>) userService.load(FILE_NAME);
-        List<User> AccountList = new ArrayList<>();
-        for (User user : users) {
-            if (user.getRole() == login.getChoice()) {
-                AccountList.add(user);
-            }
-        }
-
-        for (int i = 0; i < AccountList.size(); i++)
-        {
-            if (AccountList.get(i).getId().equals(this.loginIDAttempt))
-            {
-                if (AccountList.get(i).getPassword().equals(this.passwordAttempt))
-                {
-                    System.out.println("Login successfully.");
-                    return true;
-                }
-                else
-                {
-                    System.out.println("Incorrect Password.");
-                    return false;
-                }
-            }
-        }
-        System.out.println("LoginID not found.");
-        return false;
     }
 }

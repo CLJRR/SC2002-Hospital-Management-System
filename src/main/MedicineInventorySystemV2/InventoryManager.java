eacd85ad0505c70371f3f027eb82e1b3353f8194
JS <jiesheng100@hotmail.com>
2024-11-08 18:50:26 +0800

    Applied Solid Framework nonsense. Splitting functions into multiple class

---
+++ src/main/MedicineInventorySystemV2/InventoryManager.java
@@ -0,0 +1,54 @@
+package MedicineInventorySystemV2;
+
+import TODO.MedicationInventory;
+import java.util.*;
+
+public class InventoryManager {
+    private Map<String, MedicationInventory> inventory;
+    private InventoryLoader inventoryLoader;
+    private InventorySaver inventorySaver;
+    private InventoryViewer inventoryViewer;
+    private StockAdjuster stockAdjuster;
+    private LowStockChecker lowStockChecker;
+
+    // Constructor
+    public InventoryManager() {
+        this.inventory = new HashMap<>();
+        this.inventoryLoader = new InventoryLoader(this.inventory);
+        this.inventorySaver = new InventorySaver(this.inventory);
+        this.inventoryViewer = new InventoryViewer(this.inventory);
+        this.stockAdjuster = new StockAdjuster(this.inventory);
+        this.lowStockChecker = new LowStockChecker(this.inventory);
+
+        // Load initial inventory
+        inventoryLoader.loadInitialInventory();
+    }
+
+    public void saveInventory() {
+        inventorySaver.saveInventory();
+    }
+
+    public void addMedication(MedicationInventory medication) {
+        inventory.put(medication.getName(), medication);
+    }
+
+    public void viewInventory() {
+        inventoryViewer.viewInventory();
+    }
+
+    public void increaseStock(String medicationName, int quantity) {
+        stockAdjuster.increaseStock(medicationName, quantity);
+    }
+
+    public void decreaseStock(String medicationName, int quantity) {
+        stockAdjuster.decreaseStock(medicationName, quantity);
+    }
+
+    public void checkLowStockAlerts() {
+        lowStockChecker.checkLowStockAlerts();
+    }
+
+    public Map<String, MedicationInventory> getInventory() {
+        return inventory;
+    }
+}
---
+++ src/main/MedicineInventorySystemV2/InventorySaver.java
@@ -0,0 +1,20 @@
+package MedicineInventorySystemV2;
+
+
+import TODO.MedicationInventory;
+import java.util.*;
+
+public class InventorySaver {
+    private Map<String, MedicationInventory> inventory;
+    private MedicineInventoryService inventoryService;
+
+    public InventorySaver(Map<String, MedicationInventory> inventory) {
+        this.inventory = inventory;
+        this.inventoryService = new MedicineInventoryService();
+    }
+
+    public void saveInventory() {
+        List<MedicationInventory> inventoryList = new ArrayList<>(inventory.values());
+        inventoryService.saveInventory(inventoryList);
+    }
+}

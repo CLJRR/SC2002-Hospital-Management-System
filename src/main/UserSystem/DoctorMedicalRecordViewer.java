43a2561c5e3b93eff567b0c457bf44c963a0030c
exkitty1 <ngziyunalol@gmail.com>
2024-11-13 23:25:03 +0800

    minor edits for formatting

--- src/main/UserSystem/DoctorMedicalRecordViewer.java
+++ src/main/UserSystem/DoctorMedicalRecordViewer.java
@@ -7,7 +7,7 @@ public class DoctorMedicalRecordViewer {
     private User doctor;
     private GetUser getUser;
     private Map<String, AppointmentOutcomeRecord> appointmentOutcomeRecords;
-    
+
     public DoctorMedicalRecordViewer(String UserId, Map<String, AppointmentOutcomeRecord> appointmentOutcomeRecords) {
         this.appointmentOutcomeRecords = appointmentOutcomeRecords;
         this.getUser = new GetUser();
@@ -15,9 +15,34 @@ public class DoctorMedicalRecordViewer {
     }
 
     public void viewMedicalRecords() {
+        Map<String, User> patients = new HashMap<>();
         for (AppointmentOutcomeRecord record : appointmentOutcomeRecords.values()) {
             if (record.getDoctorId().equals(doctor.getId())) {
-                System.out.println(record.toString());
+                User patient = getUser.getUser(record.getPatientId());
+                patients.put(record.getPatientId(), patient);
+            }
+        }
+        for (User patient : patients.values()) {
+            if (patient != null) {
+                System.out.println("Patient: " + patient.getId());
+                System.out.println("Name: " + patient.getName());
+                System.out.println("Date of Birth: " + patient.getDateOfBirth());
+                System.out.println("Gender: " + patient.getGender());
+                System.out.println("Phone Number: " + patient.getPhoneNumber());
+                System.out.println("Email: " + patient.getEmail());
+                System.out.println("Blood Type: " + patient.getBloodType());
+                String diagnoses = "";
+                String treatments = "";
+                for (AppointmentOutcomeRecord record : appointmentOutcomeRecords.values()) {
+                    if (record.getPatientId().equals(patient.getId())) {
+                        diagnoses = diagnoses + record.getDiagnoses() + " ";
+                        treatments = treatments + record.getPrescription().getMedName() + ", "
+                                + record.getPrescription().getAmount() + ", " + record.getPrescription().getDosage()
+                                + " ";
+                    }
+                }
+                System.out.println("Diagnoses: " + diagnoses);
+                System.out.println("Treatments: " + treatments);
             }
         }
     }

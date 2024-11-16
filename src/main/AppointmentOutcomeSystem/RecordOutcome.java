1271d67f824eb6cf1764a41178457ae799da9420
CLJR <calvinleejiarong@gmail.com>
2024-11-16 15:55:39 +0800

    fixed recursive issue wtih recordOutcome

--- src/main/AppointmentOutcomeSystem/RecordOutcome.java
+++ src/main/AppointmentOutcomeSystem/RecordOutcome.java
@@ -1,34 +1,37 @@
 package AppointmentOutcomeSystem;
 
-import AppointmentSystem.Appointment;
-import AppointmentSystem.AppointmentController;
+import AppointmentSystem.*;
 import MedicineInventorySystem.*;
 import SessionManager.Session;
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
 public class RecordOutcome {
 
     private Map<String, AppointmentOutcomeRecord> appointmentOutcomeRecords;
-    private AppointmentController appointmentController;
+
+    private AppointmentService appointmentService = new AppointmentService();
+    @SuppressWarnings("unchecked")
+
+    // private AppointmentController appointmentController;
     private InventoryController inventoryController;
     static final Scanner sc = new Scanner(System.in);
 
     public RecordOutcome(Map<String, AppointmentOutcomeRecord> appointmentOutcomeRecords) {
         this.appointmentOutcomeRecords = appointmentOutcomeRecords;
-        this.appointmentController = new AppointmentController();
         this.inventoryController = new InventoryController();
     }
 
     public void recordOutcome() throws IOException {
         AppointmentOutcomeRecord newRecord = prompts();
+
         if (newRecord != null) {
             if (appointmentOutcomeRecords.containsKey(newRecord.getApptId())) {
-                System.out.println("Record creation skipped due to duplicate Appointment ID.");
+                System.out.println("Record creation cancelled");
             } else {
                 appointmentOutcomeRecords.put(newRecord.getApptId(), newRecord);
-
                 System.out.println("New appointment outcome record saved successfully.");
             }
         } else {
@@ -38,20 +41,32 @@ public class RecordOutcome {
 
     public AppointmentOutcomeRecord prompts() throws IOException {
         String apptId = null;
+        @SuppressWarnings("unchecked")
+        List<Appointment> AppointmentList = (List<Appointment>) appointmentService.load();
         Appointment findRecord = null;
         //get appointment
         while (true) {
             System.out.println("Enter AppointmentId: ");
             apptId = sc.next();
-            findRecord = appointmentController.getAppointmentById(apptId);
+            findRecord = containsAppointmentId(AppointmentList, apptId);
             // Check if Id exists in appt database
+            if (apptId.equalsIgnoreCase("x")) {
+                break;
+            }
             if (findRecord == null) {
                 System.out.println("Appointment ID " + apptId + " does not exist .");
+                System.out.println("Press x to cancel");
                 continue;
+            }
+            if (!findRecord.getDoctorId().equalsIgnoreCase(Session.getLoginID())) {
+                System.out.println("Appointment is under Doctor " + findRecord.getDoctorId());
+                continue;
+
             }
             // Check for duplicate appointment ID
             if (appointmentOutcomeRecords.containsKey(apptId)) {
                 System.out.println("A Outcome record for Appointment ID " + apptId + " already exists.");
+                System.out.println("Press x to cancel");
                 continue; // Exit without creating a new record
             }
             break;
@@ -87,5 +102,14 @@ public class RecordOutcome {
                 diagnosis, // Diagnosis
                 prescription // Prescription object
         );
+    }
+
+    private Appointment containsAppointmentId(List<Appointment> appointments, String appointmentId) {
+        for (Appointment appointment : appointments) {
+            if (appointment.getAppointmentId().equals(appointmentId)) {
+                return appointment; // Found the appointment
+            }
+        }
+        return null; // Not found
     }
 }

cc793bf092ae5ca3a3fb82dd606e9263566a02a1
CLJR <calvinleejiarong@gmail.com>
2024-11-19 23:22:21 +0800

    idk

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,17 +1,21 @@
+
+
 import Interfaces.LoginUI;
 import java.io.IOException;
 
 /**
- * The {@code Main} class serves as the entry point to the application.
- * It continuously invokes the login user interface to allow users to log in.
+ * The {@code Main} class serves as the entry point to the application. It
+ * continuously invokes the login user interface to allow users to log in.
  */
 public class Main {
 
     /**
-     * The main method initializes the application and enters a loop to display the login UI.
+     * The main method initializes the application and enters a loop to display
+     * the login UI.
      *
      * @param args command-line arguments (not used in this application).
-     * @throws IOException if an input or output exception occurs during the execution of the login UI.
+     * @throws IOException if an input or output exception occurs during the
+     * execution of the login UI.
      */
     public static void main(String[] args) throws IOException {
         LoginUI x = new LoginUI();
